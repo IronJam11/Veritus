16ce6de2ce63f4f285c7664f6ad7ca0383645d05
IronJam11 <aaryanjain888@gmail.com>
2025-08-03 20:18:29 +0530

    add error and struct utils

---
+++ hardhat/contracts/utils/errors.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+error InvalidInput();
+error NoRoyaltiesToDistribute();
+error OnlyPlatformOwner();
+error NoClaimableRoyalties();
+error NotCreatorOwner();
+error TransferFailed();
+error CreatorAlreadyRegistered();
